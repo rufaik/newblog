72614a5e514114a7bb9ff12b5693f7617ef76451
System Administrator <rufai@hotmail.co.uk>
2020-05-26 07:01:45 +0100

    adding project

---
+++ src/screens/SetUp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, StyleSheet, View, Button } from 'react-native';
+
+const ListScreen = () => {
+	return(
+		<View>
+			<Text>List Screen</Text>
+		</View>
+		);
+};
+
+const styles = StyleSheet.create({});
+
+export default ListScreen;
